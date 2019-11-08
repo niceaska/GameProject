8b234de51e65abf92e4eeec675a8ab81ecc1b435
niceaska <niceaska@gmail.com>
2019-11-08 21:48:15 +0300

    style changes and some animations

--- app/src/main/java/ru/niceaska/gameproject/presentation/view/MessageAppearItemAnimator.java
+++ app/src/main/java/ru/niceaska/gameproject/presentation/view/MessageAppearItemAnimator.java
@@ -1,12 +1,16 @@
 package ru.niceaska.gameproject.presentation.view;
 
 import android.animation.ObjectAnimator;
+import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MessageAppearItemAnimator extends DefaultItemAnimator {
+
     @Override
     public boolean animateAdd(RecyclerView.ViewHolder holder) {
         if (holder instanceof MessagesAdapter.MessageViewHolder) {
@@ -15,7 +19,26 @@ public class MessageAppearItemAnimator extends DefaultItemAnimator {
             objectAnimator.setInterpolator(new AccelerateInterpolator());
             objectAnimator.start();
             return true;
+        } else if (holder instanceof MessagesAdapter.ButtonChoicesViewHolder) {
+            holder.itemView.setTranslationX(-1000f);
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(holder.itemView, "translationX", -1000f, 0f);
+            objectAnimator.setDuration(1500);
+            objectAnimator.setInterpolator(new AccelerateInterpolator());
+            objectAnimator.start();
+            return true;
         }
         return super.animateAdd(holder);
+    }
+
+    @Override
+    public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @Nullable ItemHolderInfo postLayoutInfo) {
+        if (viewHolder instanceof MessagesAdapter.MessageViewHolder) {
+            viewHolder.itemView.setVisibility(View.GONE);
+            return true;
+        } else if (viewHolder instanceof MessagesAdapter.ButtonChoicesViewHolder) {
+            viewHolder.itemView.setVisibility(View.GONE);
+            return true;
+        }
+        return super.animateDisappearance(viewHolder, preLayoutInfo, postLayoutInfo);
     }
 }
